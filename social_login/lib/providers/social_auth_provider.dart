2b17a7ad9125ffb1e6fb12c70e9e8874b5d7c492
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-24 02:10:08 +0900

    implement social login

---
+++ social_login/lib/providers/social_auth_provider.dart
@@ -0,0 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+enum AuthStatus {
+  uninitialized,
+  authenticated,
+  authenticating,
+  authenticateError,
+  authenticateException,
+  authenticateCanceled,
+}
+
+enum SocialLogins {
+  google,
+  apple,
+}
+
+class SocialAuthProvider extends ChangeNotifier {
+
+  final GoogleSignIn googleSignIn;
+  final FirebaseAuth firebaseAuth;
+
+  AuthStatus _status = AuthStatus.uninitialized;
+  AuthStatus get status => _status;
+
+  User? _currentUser;
+  User? get currentUser => _currentUser;
+
+  SocialAuthProvider({
+    required this.firebaseAuth,
+    required this.googleSignIn,
+  }){}
+
+  Future<void> handleSocialSignIn(SocialLogins social) async {
+    switch(social){
+      case SocialLogins.google:
+        await handleGoogleSignIn();
+        break;
+      case SocialLogins.apple:
+        await handleAppleSignIn();
+        break;
+    }
+  }
+
+
+  Future<bool> handleGoogleSignIn() async {
+    _setStatus(AuthStatus.authenticating);
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser == null) {
+      _setStatus(AuthStatus.authenticateCanceled);
+      return false;
+    }
+
+    GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    User? firebaseUser;
+    try {
+      firebaseUser = (await firebaseAuth.signInWithCredential(
+          credential)).user;
+    } catch (error, stacktrace) {
+      debugPrint('Authentication Error : $error\n$stacktrace');
+      _setStatus(AuthStatus.authenticateError);
+      return false;
+    }
+
+    if (firebaseUser == null){
+      _setStatus(AuthStatus.authenticateError);
+      return false;
+    }
+
+    _currentUser = firebaseUser;
+    _setStatus(_status = AuthStatus.authenticated);
+    return true;
+  }
+
+  Future<bool> handleAppleSignIn() async {
+    _setStatus(AuthStatus.authenticating);
+
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+    );
+
+    final oauthCredential = OAuthProvider('apple.com').credential(
+      idToken: appleCredential.identityToken,
+      accessToken: appleCredential.authorizationCode,
+    );
+
+    User? firebaseUser;
+    try{
+      firebaseUser = (await firebaseAuth.signInWithCredential(oauthCredential)).user;
+    } catch (error, stacktrace) {
+      debugPrint('Authentication Error : $error\n$stacktrace');
+      _setStatus(AuthStatus.authenticateError);
+      return false;
+    }
+
+    if (firebaseUser == null){
+      _setStatus(AuthStatus.authenticateError);
+      return false;
+    }
+
+    _currentUser = firebaseUser;
+    _setStatus(_status = AuthStatus.authenticated);
+    return true;
+  }
+
+  Future<void> handleSignOut() async {
+    await firebaseAuth.signOut();
+    // Note: Do this to prevent google sign-in from automatically selecting the previous account.
+    // See also : https://www.deepl.com/write#en/Note%20%3A%20Do%20this%20to%20prevent%20to%20google%20automatically%20choose%20previous%20account
+    if(await googleSignIn.isSignedIn()){
+      await googleSignIn.disconnect();
+      await googleSignIn.signOut();
+    }
+
+    _currentUser = null;
+    _setStatus(AuthStatus.uninitialized);
+  }
+
+  void _setStatus(AuthStatus status){
+    _status = status;
+    notifyListeners();
+  }
+
+}
---
+++ social_login/lib/views/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_login_buttons/social_login_buttons.dart';
+
+import '../providers/social_auth_provider.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late SocialAuthProvider authProvider;
+
+  @override
+  void initState() {
+    authProvider = context.read<SocialAuthProvider>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Social Logins'), // Replace with your app name
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildSignInUI(),
+            const SizedBox(height: 30),
+            SocialLoginButton(
+              buttonType: SocialLoginButtonType.google,
+              onPressed: () async {
+                await authProvider.handleGoogleSignIn();
+              },
+            ),
+            const SizedBox(height: 30),
+            SocialLoginButton(
+              buttonType: SocialLoginButtonType.apple,
+              onPressed: () async {
+                await authProvider.handleAppleSignIn();
+              },
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () async {
+                // Place your sign-out logic here
+                await authProvider.handleSignOut(); // Assuming authProvider has a signOut method
+              },
+              child: const Text(
+                'Sign Out',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInUI(){
+    return Consumer<SocialAuthProvider>(
+      builder: (context, authProvider, child) {
+        final AuthStatus signInStatus = authProvider.status;
+        final User? user = authProvider.currentUser;
+
+        if (signInStatus == AuthStatus.authenticating){
+          return const CircularProgressIndicator(
+            color: Colors.purple,
+          );
+        }
+
+        if (user == null){
+          return const Text(
+            "You are not signed-in.",
+            style: TextStyle(fontSize: 30),
+          );
+        }
+
+        return Text(
+          "You are signed-in.\n"
+          "Name : ${user.displayName}\n"
+          "Email : ${user.email}",
+          style: const TextStyle(fontSize: 15),
+        );
+      },
+    );
+  }
+}
