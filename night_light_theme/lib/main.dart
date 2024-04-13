74256cf0744fdcc0e38ec08923e3c84a24907334
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-14 00:31:19 +0900

    refactor shared preference

--- night_light_theme/lib/main.dart
+++ night_light_theme/lib/main.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:night_light_theme/providers/theme_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 
 import 'views/home_page.dart';
 
-Future<SharedPreferences> _initSharedPreference() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  return await SharedPreferences.getInstance();
-}
-
 
 Future<void> main() async {
-  final SharedPreferences prefs = await _initSharedPreference();
 
-  runApp(MyApp(
-    prefs: prefs,
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({
     super.key,
-    required this.prefs
   });
-
-  final SharedPreferences prefs;
 
   @override
   Widget build(BuildContext context) {
