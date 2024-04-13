2dfe905f3bb33f84e392cb19f210ed7abe5eb5d3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-13 20:48:08 +0900

    update UI

--- night_light_theme/lib/views/home_page.dart
+++ night_light_theme/lib/views/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../providers/theme_provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,39 +12,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late ThemeProvider themeProvider;
+
+  @override
+  void initState() {
+    themeProvider = context.read<ThemeProvider>();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    themeProvider = context.watch<ThemeProvider>();
+    final attrs = themeProvider.themeAttrs;
     return Scaffold(
+      backgroundColor: attrs.backgroundColor,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Light/Night Theme Sample"),
+        backgroundColor: attrs.backgroundColor,
+        title: Text(
+          "Light/Night Theme Sample",
+          style: TextStyle(color: attrs.fontColor),
+        ),
+        elevation: 4.0,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              attrs.themeName,
+              style: GoogleFonts.fredoka(
+                textStyle: TextStyle(
+                  color: attrs.fontColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Icon(
+              attrs.themeIcon,
+              size: 100,
+              color: attrs.fontColor,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Toggle Theme'),
+              onPressed: () {
+                themeProvider.toggleTheme();
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
