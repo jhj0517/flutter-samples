081826a982790705987fba286bd8537914accbde
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-13 23:24:48 +0900

    refactor to use theme data

--- night_light_theme/lib/providers/theme_provider.dart
+++ night_light_theme/lib/providers/theme_provider.dart
@@ -1,75 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import '../constants/app_constants.dart';
-
-enum Themes{
-  light,
-  dark
-}
 
 class ThemeProvider extends ChangeNotifier {
 
-  ThemeProvider({
-    required this.prefs
-  }){
-    _setTheme();
-  }
+  ThemeProvider(){}
 
-  final SharedPreferences prefs;
-  final initialTheme = Themes.light;
+  ThemeData _themeColors = ThemeColors.light;
+  ThemeData get themeColors => _themeColors;
 
-  Themes? _them;
-  Themes? get theme => _them;
-
-  ThemeAttrs get themeAttrs => _getThemeAttrs();
+  ThemeAttrs _attrs = LightThemeAttrs();
+  ThemeAttrs get attrs => _attrs;
 
   void toggleTheme() {
-    _them = _them == Themes.light ? Themes.dark : Themes.light;
-    _saveTheme();
+    bool isLight = _themeColors == ThemeColors.light;
+    _themeColors = isLight ? ThemeColors.dark : ThemeColors.light;
+    _attrs = isLight ? DarkThemeAttrs() : LightThemeAttrs();
+
     notifyListeners();
-  }
-
-  void _setTheme() {
-    int? themeIndex = prefs.getInt(AppConstants.themeKey);
-    _them = themeIndex == null ? initialTheme : Themes.values[themeIndex];
-  }
-
-  void _saveTheme() {
-    prefs.setInt(AppConstants.themeKey, _them!.index);
-  }
-
-  ThemeAttrs _getThemeAttrs() {
-    return _them == Themes.light
-        ? LightTheme()
-        : DarkTheme();
   }
 
 }
 
+class ThemeColors {
+  static ThemeData light = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    colorScheme: const  ColorScheme.light(
+      background: Color(0xFFFFFFFF),
+      primary: Color(0xFF000000),
+      secondary: Color(0xFFE1A7FF),
+    ),
+  );
+
+  static ThemeData dark = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme.dark(
+      background: Color(0xFF000000),
+      primary: Color(0xFFFFFFFF),
+      secondary: Color(0xFFE1A7FF),
+    ),
+  );
+}
+
+// Attribute class for other things instead of colors
 abstract class ThemeAttrs {
-  Color get backgroundColor;
-  Color get fontColor;
   String get themeName;
   IconData get themeIcon;
 }
 
-class LightTheme implements ThemeAttrs {
-  @override
-  Color get backgroundColor => const Color(0xffffffff);
-  @override
-  Color get fontColor => const Color(0xFF000000);
+class LightThemeAttrs implements ThemeAttrs {
   @override
   String get themeName => "Light Theme";
   @override
   IconData get themeIcon => Icons.light_mode_outlined;
 }
 
-class DarkTheme implements ThemeAttrs {
-  @override
-  Color get backgroundColor => const Color(0xff000000);
-  @override
-  Color get fontColor => const Color(0xFFFFFFFF);
+class DarkThemeAttrs implements ThemeAttrs {
   @override
   String get themeName => "Dark Theme";
   @override
