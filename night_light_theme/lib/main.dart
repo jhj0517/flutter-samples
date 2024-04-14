28c26b36f1c59a51d3f576fc5f07121a26ce1417
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-14 13:01:14 +0900

    init shared preference

--- night_light_theme/lib/main.dart
+++ night_light_theme/lib/main.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:night_light_theme/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'views/home_page.dart';
 
 
-Future<void> main() async {
+Future<SharedPreferences> _initSharedPreference() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  return await SharedPreferences.getInstance();
+}
 
-  runApp(const MyApp());
+Future<void> main() async {
+  final prefs = await _initSharedPreference();
+  runApp(MyApp(
+    prefs: prefs,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({
     super.key,
+    required this.prefs,
   });
+
+  final SharedPreferences prefs;
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<ThemeProvider>(
-          create: (context) => ThemeProvider()
+          create: (context) => ThemeProvider(
+            prefs: prefs
+          )
         ),
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, child) {
           return MaterialApp(
             title: 'Light/Night Theme Sample',
-            theme: themeProvider.theme,
+            theme: themeProvider.attrs.themeColors,
             home: const MyHomePage(),
           );
         },
