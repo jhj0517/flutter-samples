cee7f6ea6ef22edd354b1310220189c1677e00d1
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-14 23:48:03 +0900

    add service class

---
+++ modify_png_metadata/lib/core/services/image_service.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+
+class ImageService{
+
+  static Future<File?> pickImage({
+    required BuildContext context
+  }) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile == null) {
+      return null;
+    }
+    return File(pickedFile.path);
+  }
+
+  static Future<Uint8List> toBLOB({
+    required File imageFile
+  }) async {
+    Uint8List imageBytes = await imageFile.readAsBytes();
+    return imageBytes;
+  }
+
+  static Future<void> saveToGallery({
+    required String filePath,
+  }) async {
+    String timestamp = DateFormat('yyyyMMdd_HHmmss').format(DateTime.now());
+    String fileName = "Modify_PNG_Metadata_Sample_$timestamp";
+    await ImageGallerySaver.saveFile(
+      filePath,
+      name: fileName,
+    );
+  }
+
+}
