9dab6773f3c382086ddfd45a4b22b9c346736dbc
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-14 23:55:17 +0900

    add png chunk service

---
+++ modify_png_metadata/lib/core/services/png_chunk_service.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:path/path.dart' as p;
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:png_chunks_encode/png_chunks_encode.dart' as pngEncode;
+import 'package:png_chunks_extract/png_chunks_extract.dart' as pngExtract;
+
+class PngChunkService{
+
+  static List<Map<String, dynamic>>? readChunk({
+    required Uint8List BLOB
+  }) {
+    try{
+      return pngExtract.extractChunks(BLOB);
+    } catch(e){
+      debugPrint("Reading Chunk Failed $e");
+      return null;
+    }
+  }
+
+}
