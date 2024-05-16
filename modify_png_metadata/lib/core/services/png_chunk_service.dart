336337cc4be91a39d8430a6d093ed645481fffa3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-16 21:28:39 +0900

    add chunk util function

--- modify_png_metadata/lib/core/services/png_chunk_service.dart
+++ modify_png_metadata/lib/core/services/png_chunk_service.dart
@@ -1,15 +1,12 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
 import 'dart:convert';
-import 'package:path/path.dart' as p;
-import 'package:image_gallery_saver/image_gallery_saver.dart';
-import 'package:png_chunks_encode/png_chunks_encode.dart' as pngEncode;
+import 'package:png_chunks_encode/src/etc32.dart';
 import 'package:png_chunks_extract/png_chunks_extract.dart' as pngExtract;
 
 class PngChunkService{
 
-  static List<Map<String, dynamic>>? readChunk({
+  static List<Map<String, dynamic>>? read({
     required Uint8List BLOB
   }) {
     try{
@@ -18,6 +15,40 @@ class PngChunkService{
       debugPrint("Reading Chunk Failed $e");
       return null;
     }
+  }
+
+  static List<Map<String, dynamic>>? gettEXt({
+    required Uint8List BLOB
+  }) {
+    final chunk = read(BLOB: BLOB);
+    if (chunk==null){
+      return null;
+    }
+
+    return chunk.where((c) => c["name"]=="tEXt").toList();
+  }
+
+
+  static List<Map<String, dynamic>> addtEXt({
+    required List<Map<String, dynamic>> chunk,
+    required String text,
+  }) {
+    List<int> newData = [...utf8.encode(text)];
+
+    Uint8List chunkType = Uint8List.fromList(utf8.encode('tEXt'));
+    Uint8List dataBytes = Uint8List.fromList(newData);
+    Uint8List crcInput = Uint8List.fromList([...chunkType, ...dataBytes]);
+    int crc = Crc32.getCrc32(crcInput);
+
+    Map<String, dynamic> tEXtChunk = {
+      'name': 'tEXt',
+      'data': dataBytes,
+      'crc': crc
+    };
+
+    int end = chunk.indexWhere((chunk) => chunk['name'] == 'IEND');
+    chunk.insert(end, tEXtChunk);
+    return chunk;
   }
 
 }
