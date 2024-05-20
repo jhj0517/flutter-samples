6a4d5cd5bc35b915627c32e8b1b9812b66fcdd80
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-21 00:11:47 +0900

    update metadata output view to table

--- modify_png_metadata/lib/presentation/views/home/widgets/meta_data_output.dart
+++ modify_png_metadata/lib/presentation/views/home/widgets/meta_data_output.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:modify_png_metadata/core/services/png_chunk_service.dart';
 import 'package:modify_png_metadata/presentation/providers/home_provider.dart';
@@ -12,9 +14,54 @@ class MetaDataOutput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final metadata = context.select<HomeProvider, List<Map<String, dynamic>>?>((provider) => provider.chunks);
-    return Text(
-      "${PngChunkService.gettEXt(chunk: metadata ?? [])}",
-      style: const TextStyle(fontSize: 20, color: Colors.grey),
+    if (metadata == null || metadata.isEmpty) {
+      return const Text(
+        "No metadata available",
+        style: TextStyle(fontSize: 20, color: Colors.grey),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Metadata Inspector",
+          textAlign: TextAlign.left,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: const [
+              DataColumn(label: Text('Type')),
+              DataColumn(label: Text('Length')),
+              DataColumn(label: Text('Data')),
+            ],
+            rows: metadata.map((chunk) {
+              final type = chunk['name'];
+              final data = chunk['data'];
+              final length = data.length.toString();
+
+              String displayData;
+              switch(type){
+                case 'IDAT':
+                  displayData = '[${data.take(10)} .... ]';
+                case 'tEXt':
+                  final tEXt = PngChunkService.gettEXt(chunk: metadata)![0];
+                  displayData = "keyword : ${tEXt['keyword']}\nText String : ${tEXt['data']}";
+                default:
+                  displayData = data.toString();
+              }
+
+              return DataRow(cells: [
+                DataCell(Text(type)),
+                DataCell(Text(length)),
+                DataCell(Text(displayData)),
+              ]);
+            }).toList(),
+          ),
+        )
+      ],
     );
   }
 }
