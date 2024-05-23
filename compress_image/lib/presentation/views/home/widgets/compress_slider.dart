7d5a4ce472a7d2d16d27a0cbcc223ebab5463741
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-23 22:28:21 +0900

    add image quality slider

---
+++ compress_image/lib/presentation/views/home/widgets/compress_slider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:compress_image/presentation/providers/home_provider.dart';
+
+class CompressSlider extends StatelessWidget {
+
+  const CompressSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final imageQuality = context.select<HomeProvider, double>((provider) => provider.imageQuality);
+
+    return Column(
+      children: [
+        Text("Image Quality : $imageQuality}"),
+        Slider(
+          value: imageQuality,
+          min: 0,
+          max: 100,
+          divisions: 100,
+          label: "Quality : ${imageQuality.round().toString()}",
+          onChanged: (value) {
+            final homeProvider= Provider.of<HomeProvider>(context, listen:false);
+            homeProvider.setImageQuality(value);
+          },
+        )
+      ],
+    );
+  }
+}
